2e403a61cab5f1949b71fbfc7e654d14cdf873d3
pauldin91 <pauldin91@gmail.com>
2026-09-07 10:39:39 +0300

    format

--- src/WebApi/Aggregation.Backend.WebApi/Program.cs
+++ src/WebApi/Aggregation.Backend.WebApi/Program.cs
@@ -13,7 +13,7 @@ try
 
     var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+    // Add services to the container.
     builder.Host.UseSerilog(new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger());
 
     builder.Services.AddApplicationExtensions();
@@ -26,16 +26,14 @@ try
     app.Configuration.Bind(nameof(ExternalIdProviderOptions), extIdOptions);
 
     app.UseExceptionHandler("/Error");
-// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Aggregation API v1");
-
         c.OAuthClientId(extIdOptions.ClientId);
-        c.OAuth2RedirectUrl("https://localhost:7064/swagger/oauth2-redirect.html");
         c.OAuthScopes("read:user", "user:email");
         c.OAuthUsePkce();
     });
