7f1a27c55e68f544fcabad8915950aa520b5324d
pauldin91 <pauldin91@gmail.com>
2026-09-11 16:46:58 +0300

    vite cfg

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5164,
+    proxy: {
+      '/auth': 'http://localhost:5146',
+      '/api': 'http://localhost:5146',
+    }
+  }
 })
